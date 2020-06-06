31d146c615bbad97a67a8a6e46a0667ecd02da30
Mikail Khan <mikail.khan45@gmail.com>
2020-06-05 22:44:40 -0400

    started working on shape create

--- src/resources.rs
+++ src/resources.rs
@@ -1,4 +1,5 @@
-use crate::Vector;
+use crate::{Vector, Point};
+use ncollide2d;
 
 #[derive(Copy, Clone)]
 pub struct MousePos(pub Vector);
@@ -11,3 +12,31 @@ impl Default for MousePos {
 
 #[derive(Copy, Clone, Default)]
 pub struct DT(pub std::time::Duration);
+
+#[derive(Copy, Clone)]
+pub enum ShapeInfo {
+    Rectangle(Vector),
+    Circle(f32),
+}
+
+#[derive(Copy, Clone)]
+pub struct CreateShapeData {
+    pub shape: ShapeInfo,
+    pub pos: Point,
+    pub centered: bool,
+}
+
+#[derive(Copy, Clone)]
+pub struct CreationData {
+    creating: bool,
+    shape_data: Option<CreateShapeData>,
+}
+
+impl Default for CreationData {
+    fn default() -> Self {
+        CreationData {
+            creating: false,
+            shape_data: None,
+        }
+    }
+}
