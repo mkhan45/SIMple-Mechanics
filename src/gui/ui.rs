cce22eb98675809c08d911a56db200176916f6c5
Mikail Khan <mikail.khan45@gmail.com>
2020-07-12 14:42:36 -0400

    modify object properties through menu

--- src/gui/ui.rs
+++ src/gui/ui.rs
@@ -2,14 +2,15 @@ use imgui::*;
 
 use crate::gui::imgui_wrapper::*;
 use crate::{
-    components::PhysicsBody,
+    components::{Collider, PhysicsBody},
     resources::{
         CreateElasticity, CreateFriction, CreateMass, CreateShapeCentered, CreateShapeStatic,
         Resolution, ShapeInfo,
     },
-    BodySet, RigidBody,
+    BodySet, ColliderSet, RigidBody,
 };
 
+use nphysics2d::material::BasicMaterial;
 use specs::prelude::*;
 
 macro_rules! signal_button {
@@ -85,19 +86,23 @@ pub fn make_sidemenu(
     entity: Entity,
     signals: &mut Vec<UiSignal>,
 ) {
-    let body_set = world.fetch::<BodySet>();
+    let mut body_set = world.fetch_mut::<BodySet>();
     let physics_body = {
         let physics_bodies = world.read_storage::<PhysicsBody>();
         let physics_body_handle = physics_bodies.get(entity).unwrap();
         body_set
-            .get(physics_body_handle.body_handle)
+            .get_mut(physics_body_handle.body_handle)
             .unwrap()
-            .downcast_ref::<RigidBody>()
+            .downcast_mut::<RigidBody>()
             .unwrap()
     };
 
-    let pos = physics_body.position();
-    let vel = physics_body.velocity();
+    let mut collider_set = world.fetch_mut::<ColliderSet>();
+    let body_collider = {
+        let colliders = world.read_storage::<Collider>();
+        let collider_handle = colliders.get(entity).unwrap();
+        collider_set.get_mut(collider_handle.coll_handle).unwrap()
+    };
 
     let resolution = world.fetch::<Resolution>().0;
     let win = imgui::Window::new(im_str!("Object Info"))
@@ -114,6 +119,9 @@ pub fn make_sidemenu(
         .movable(false);
 
     win.build(ui, || {
+        let pos = physics_body.position();
+        let vel = physics_body.velocity();
+
         ui.text(im_str!("\nObject Info"));
         ui.text(format!(
             "Position: {:.2}, {:.2}",
@@ -125,6 +133,28 @@ pub fn make_sidemenu(
             vel.linear.x, vel.linear.y
         ));
         ui.text(format!("Angular Velocity: {:.2}", vel.angular));
+
+        let mut mass = physics_body.augmented_mass().linear;
+        ui.drag_float(im_str!("Mass"), &mut mass)
+            .min(0.0)
+            .max(250.0)
+            .speed(0.25)
+            .build();
+        physics_body.set_mass(mass);
+
+        let material = body_collider.material_mut();
+        let basic_material = material.downcast_mut::<BasicMaterial<f32>>().unwrap();
+        ui.drag_float(im_str!("Friction"), &mut basic_material.friction)
+            .min(0.0)
+            .max(1.0)
+            .speed(0.1)
+            .build();
+
+        ui.drag_float(im_str!("Elasticity"), &mut basic_material.restitution)
+            .min(0.0)
+            .max(1.0)
+            .speed(0.1)
+            .build();
 
         signal_button!("Delete Shape", UiSignal::DeleteShape(entity), ui, signals);
     });
