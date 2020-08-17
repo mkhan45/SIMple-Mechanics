bc2b5fa67a9adfb5184b9dba3d43981e63954076
Mikail Khan <mikail.khan45@gmail.com>
2020-08-17 02:35:25 -0400

    removed elasticity and friction from can-it lab so that rotation doesn't matter

--- lua/can-it-lab.lua
+++ lua/can-it-lab.lua
@@ -1,17 +1,17 @@
 add_shapes(
     {shape = "rect", status = "static", x = SCREEN_X / 2, y = SCREEN_Y, 
-    w = SCREEN_X, h = 0.25, mass = 1, elasticity = 0, friction = 1},
+    w = SCREEN_X, h = 0.25, mass = 1, elasticity = 0, friction = 0},
 
     {shape = "rect", status = "static", x = 7 * SCREEN_X / 8, y = SCREEN_Y / 2, 
-    w = SCREEN_X / 8, h = 0.25, rotation = math.pi / 6, mass = 1, elasticity = 0, friction = 1},
+    w = SCREEN_X / 8, h = 0.25, rotation = math.pi / 6, mass = 1, elasticity = 0, friction = 0},
 
     {shape = "rect", status = "static", x = SCREEN_X - 0.5, y = SCREEN_Y / 2 - 0.25, 
-    w = 0.25, h = SCREEN_Y / 8, mass = 1, elasticity = 0, friction = 1},
+    w = 0.25, h = SCREEN_Y / 8, mass = 1, elasticity = 0, friction = 0},
 
-    {shape = "rect", status = "static", x = SCREEN_X / 10, y = SCREEN_Y - 1, w = 0.25, h = 1, mass = 4},
-    {shape = "rect", status = "static", x = 2 * SCREEN_X / 10, y = SCREEN_Y - 1, w = 0.25, h = 1, mass = 4},
+    {shape = "rect", status = "static", x = SCREEN_X / 10, y = SCREEN_Y - 1, w = 0.25, h = 1, mass = 4, elasticity = 0, friction = 0},
+    {shape = "rect", status = "static", x = 2 * SCREEN_X / 10, y = SCREEN_Y - 1, w = 0.25, h = 1, mass = 4, elasticity = 0, friction = 0},
 
-    {shape = "circle", x = 7.5 * SCREEN_X / 8, y = SCREEN_Y / 2 - 0.75, mass = 1, r = 0.75}
+    {shape = "circle", x = 7.5 * SCREEN_X / 8, y = SCREEN_Y / 2 - 0.75, mass = 1, r = 0.75, elasticity = 0, friction = 0}
 )
 
 GRAVITY = 9.81
